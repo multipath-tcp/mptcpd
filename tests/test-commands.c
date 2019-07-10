3f25fa813e41d7f2d063d8bd394b0923ae8b2b7a
Ossama Othman <ossama.othman@intel.com>
2019-07-10 13:50:53 -0700

    path_manager: Explicitly request "mptcp" family. (#40)
    With the new ELL generic netlink API, it is no longer enough to simply
    set a watch for the "mptcp" generic netlink family since the watch is
    only triggered on a family change (appeared or vanished).  Explicitly
    request the "mptcp" family.  This fixes a problem where mptcpd would
    not fully initialize it's path manager bridge component.

--- tests/test-commands.c
+++ tests/test-commands.c
@@ -26,7 +26,6 @@
 
 #include <mptcpd/path_manager.h>
 
-
 // -------------------------------------------------------------------
 
 static bool is_pm_ready(struct mptcpd_pm const *pm, char const *fname)
@@ -110,6 +109,14 @@ void test_get_nm(void const *test_data)
 static void run_tests(struct l_genl_family_info const *info,
                       void *user_data)
 {
+        /*
+          Check if the initial request for the "mptcp" generic netlink
+          family failed.  A subsequent family watch will be used to
+          call this function again when it appears.
+         */
+        if (info == NULL)
+                return;
+
         assert(strcmp(l_genl_family_info_get_name(info),
                       MPTCP_GENL_NAME) == 0);
 
@@ -126,6 +133,8 @@ static void timeout_callback(struct l_timeout *timeout,
 {
         (void) timeout;
         (void) user_data;
+
+        l_debug("test timed out");
 
         l_main_quit();
 }
@@ -182,12 +191,19 @@ int main(void)
 
         assert(watch_id != 0);
 
+        bool const requested = l_genl_request_family(genl,
+                                                     MPTCP_GENL_NAME,
+                                                     run_tests,
+                                                     &tests_called,
+                                                     NULL);
+        assert(requested);
+
         // Bound the time we wait for the tests to run.
         static unsigned long const milliseconds = 500;
         struct l_timeout *const timeout =
                 l_timeout_create_ms(milliseconds,
                                     timeout_callback,
-                                    &tests_called,
+                                    NULL,
                                     NULL);
 
         (void) l_main_run();
