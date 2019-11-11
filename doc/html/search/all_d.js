2e4528f84c2170b4543c042cec3bd5a5cb4f20a6
Ossama Othman <ossama.othman@intel.com>
2019-11-11 09:46:59 -0800

    doc: Update generated Doxygen docs.

--- doc/html/search/all_d.js
+++ doc/html/search/all_d.js
@@ -6,5 +6,6 @@ var searchData=
   ['plugin_2ec',['plugin.c',['../plugin_8c.html',1,'']]],
   ['plugin_2eh',['plugin.h',['../plugin_8h.html',1,'']]],
   ['plugin_5fdir',['plugin_dir',['../structmptcpd__config.html#a44337a57088993114544751bd955bcea',1,'mptcpd_config']]],
+  ['plugin_5fprivate_2eh',['plugin_private.h',['../plugin__private_8h.html',1,'']]],
   ['pm',['pm',['../structsspi__new__connection__info.html#a1ae49c34c3e042e19aeac03a8ad196e8',1,'sspi_new_connection_info']]]
 ];
--- doc/html/search/files_4.js
+++ doc/html/search/files_4.js
@@ -4,5 +4,6 @@ var searchData=
   ['path_5fmanager_2eh',['path_manager.h',['../include_2mptcpd_2path__manager_8h.html',1,'(Global Namespace)'],['../src_2path__manager_8h.html',1,'(Global Namespace)']]],
   ['path_5fmanager_5fprivate_2eh',['path_manager_private.h',['../path__manager__private_8h.html',1,'']]],
   ['plugin_2ec',['plugin.c',['../plugin_8c.html',1,'']]],
-  ['plugin_2eh',['plugin.h',['../plugin_8h.html',1,'']]]
+  ['plugin_2eh',['plugin.h',['../plugin_8h.html',1,'']]],
+  ['plugin_5fprivate_2eh',['plugin_private.h',['../plugin__private_8h.html',1,'']]]
 ];
