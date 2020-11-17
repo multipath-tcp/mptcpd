2c1dc3697e10f08316b2a83f3032ad426abb19cf
Ossama Othman <ossama.othman@intel.com>
2020-11-17 10:27:51 -0800

    plugins: Add new address advertising plugin.
    A new "addr_adv" plugin was added that dynamically advertises local
    network addresses through MPTCP as new local addresses become
    available.  Similarly, the plugin stops advertising a given network
    address once it is no longer available.

---
+++ plugins/path_managers/addr_adv.c
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/**
+ * @file addr_adv.c
+ *
+ * @brief MPTCP address advertiser path manager plugin.
+ *
+ * Copyright (c) 2020, Intel Corporation
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <mptcpd/config-private.h>
+#endif
+
+#include <ell/plugin.h>
+#include <ell/util.h>  // For L_STRINGIFY needed by ELL log macros.
+#include <ell/log.h>
+
+#include <mptcpd/id_manager.h>
+#include <mptcpd/network_monitor.h>
+#include <mptcpd/path_manager.h>
+#include <mptcpd/plugin.h>
+
+
+static struct mptcpd_idm *_idm;
+
+static void addr_adv_new_local_address(struct mptcpd_interface const *i,
+                                       struct sockaddr const *sa,
+                                       struct mptcpd_pm *pm)
+{
+        mptcpd_aid_t const id = mptcpd_idm_get_id(_idm, sa);
+
+        if (id == 0) {
+                l_error("Unable to map addr to ID.");
+                return;
+        }
+
+        uint32_t       const flags = 0;
+        mptcpd_token_t const token = 0;
+
+        if (mptcpd_pm_add_addr(pm, sa, id, flags, i->index, token) != 0)
+                l_error("Unable to advertise IP address.");
+}
+
+static void addr_adv_delete_local_address(
+        struct mptcpd_interface const *i,
+        struct sockaddr const *sa,
+        struct mptcpd_pm *pm)
+{
+        (void) i;
+
+        mptcpd_aid_t const id = mptcpd_idm_remove_id(_idm, sa);
+
+        if (id == 0) {
+                // Not necessarily an error.
+                l_info("No address ID associated with addr.");
+                return;
+        }
+
+        mptcpd_token_t const token = 0;
+
+        if (mptcpd_pm_remove_addr(pm, id, token) != 0)
+                l_error("Unable to stop advertising IP address.");
+}
+
+static struct mptcpd_plugin_ops const pm_ops = {
+        .new_local_address    = addr_adv_new_local_address,
+        .delete_local_address = addr_adv_delete_local_address
+};
+
+static int addr_adv_init(void)
+{
+        static char const name[] = "addr_adv";
+
+        if (!mptcpd_plugin_register_ops(name, &pm_ops)) {
+                l_error("Failed to initialize address advertiser "
+                        "path manager plugin.");
+
+                return -1;
+        }
+
+        _idm = mptcpd_idm_create();
+
+        l_info("MPTCP address advertiser path manager initialized.");
+
+        return 0;
+}
+
+static void addr_adv_exit(void)
+{
+        mptcpd_idm_destroy(_idm);
+
+        l_info("MPTCP address advertiser path manager exited.");
+}
+
+L_PLUGIN_DEFINE(MPTCPD_PLUGIN_DESC,
+                addr_adv,
+                "Address advertiser path manager",
+                VERSION,
+                L_PLUGIN_PRIORITY_DEFAULT,
+                addr_adv_init,
+                addr_adv_exit)
+
+
+/*
+  Local Variables:
+  c-file-style: "linux"
+  End:
+*/
