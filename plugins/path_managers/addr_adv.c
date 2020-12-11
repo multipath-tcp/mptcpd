07b9f6808e4703236d538ba43d24c97c2648a1be
Ossama Othman <ossama.othman@intel.com>
2020-12-10 21:50:47 -0800

    addr_adv: Use new mptcpd plugin API.

--- plugins/path_managers/addr_adv.c
+++ plugins/path_managers/addr_adv.c
@@ -67,8 +67,10 @@ static struct mptcpd_plugin_ops const pm_ops = {
         .delete_local_address = addr_adv_delete_local_address
 };
 
-static int addr_adv_init(void)
+static int addr_adv_init(struct mptcpd_pm *pm)
 {
+        (void) pm;
+
         static char const name[] = "addr_adv";
 
         if (!mptcpd_plugin_register_ops(name, &pm_ops)) {
@@ -85,20 +87,20 @@ static int addr_adv_init(void)
         return 0;
 }
 
-static void addr_adv_exit(void)
+static void addr_adv_exit(struct mptcpd_pm *pm)
 {
+        (void) pm;
+
         mptcpd_idm_destroy(_idm);
 
         l_info("MPTCP address advertiser path manager exited.");
 }
 
-L_PLUGIN_DEFINE(MPTCPD_PLUGIN_DESC,
-                addr_adv,
-                "Address advertiser path manager",
-                VERSION,
-                L_PLUGIN_PRIORITY_DEFAULT,
-                addr_adv_init,
-                addr_adv_exit)
+MPTCPD_PLUGIN_DEFINE(addr_adv,
+                     "Address advertiser path manager",
+                     MPTCPD_PLUGIN_PRIORITY_DEFAULT,
+                     addr_adv_init,
+                     addr_adv_exit)
 
 
 /*
