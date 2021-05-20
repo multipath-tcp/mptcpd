548ef11ef7a01090346fb9ab99e2ea142a33f3fe
Paolo Abeni <paolo.abeni@gmail.com>
2021-05-20 18:28:41 +0200

    add new binary helper to enable MPTCP on existing application (#135)
    Uses the upstream/vanilla kernel API and dll hijacking.
    
    Allow either:
    - running an unmodified binary forcing TCP -> MPTCP socket conversion
    - enable/disable TCP -> MPTCP socket conversion on existing systemd
      unit.
    
    v2 -> v3:
     - add missing copyright && license header
     - do not use magic numbers with strncmp
     - user argp parser && support --help --version arguments
    
    v1 -> v2:
     - !x -> x == 0
     - free cmd in locate_unit()
     - dprintf() instead of multiple write()
     - better dbg message
     - fixed a few unit updating issues
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

---
+++ src/mptcpwrap.c
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/**
+ * @file mptcpwrap.c
+ *
+ * @brief wrapper for socket() libcall hijacking
+ *
+ * Copyright (c) 2021, Red Hat, Inc.
+ */
+
+#include <sys/syscall.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <linux/net.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// libtool will make every symbol hidden by default
+int __attribute__((visibility("default"))) socket(int family, int type, int protocol)
+{
+	int ret, orig_protocol = protocol;
+
+	// the 'type' field may encode socket flags
+	if ((family != AF_INET && family != AF_INET6) ||
+	    (type & 0xff)  != SOCK_STREAM)
+		goto do_socket;
+
+	// socket(AF_INET, SOCK_STREM, 0) maps to TCP, too
+	if (protocol == 0 && protocol != IPPROTO_TCP)
+		goto do_socket;
+
+	protocol = IPPROTO_TCP + 256;
+
+do_socket:
+	ret = syscall(__NR_socket, family, type, protocol);
+	if (getenv("MPTCPWRAP_DEBUG") && protocol != orig_protocol)
+		fprintf(stderr, "mptcpwrap: changing socket protocol from 0x%x "
+				"to 0x%x (IPPROTO_MPTCP) for family 0x%x "
+				"type 0x%x fd %d\n", orig_protocol, protocol,
+				family, type, ret);
+	return ret;
+}
