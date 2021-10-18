d6a48d4acf5b16a51b01b64f8fd99b513b41e996
Ossama Othman <ossama.othman@intel.com>
2021-10-18 15:36:45 -0700

    tests: Expand mptcpwrap code coverage. (#169)
    * tests: Expand mptcpwrap code coverage.
    
    * tests: Add LD_PRELOAD check.
    
    * tests: Do not fail on unsupported protocols.
    
    Some platforms don't support SCTP out of the box, triggering the
    socket() call with the IPPROTO_SCTP to fail.  Ignore the error if the
    EPROTONOSUPPORT errno is set.

--- tests/mptcpwrap-tester.c
+++ tests/mptcpwrap-tester.c
@@ -14,22 +14,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <errno.h>
 
-int main()
+
+struct socket_data
 {
-        /*
-          libmptcpwrap.so should be preloaded when running this
-          program, e.g.:
+        int  domain;
+        int  type;
+        int  protocol;
+        bool expect_mptcp;
+};
 
-              LD_PRELOAD=libmptcpwrap.so ./mptcpwrap-tester
-        */
-        int const fd = socket(AF_INET, SOCK_STREAM, 0);
-        assert(fd != -1);
-
+static bool verify_protocol(int fd, bool expect_mptcp)
+{
         int protocol = 0;
         socklen_t len = sizeof(protocol);
-
-        int const expected_protocol = IPPROTO_TCP + 256;  // MPTCP-ized.
 
         int const ret = getsockopt(fd,
                                    SOL_SOCKET,
@@ -37,11 +41,80 @@ int main()
                                    &protocol,
                                    &len);
 
+        /*
+          The MPTCP-ized protocol injected by the hijacked socket()
+          call, i.e. IPPROTO_MPTCP.
+        */
+        static int const mptcp_protocol = IPPROTO_TCP + 256;
+
+        return ret == 0
+                && (expect_mptcp ? protocol == mptcp_protocol : true);
+}
+
+static void test_socket_data(struct socket_data const *data)
+{
+        int const fd = socket(data->domain, data->type, data->protocol);
+
+        if (fd == -1) {
+                if (errno == EPROTONOSUPPORT) {
+                        struct protoent const *const p =
+                                getprotobynumber(data->protocol);
+
+                        fprintf(stderr,
+                                "WARNING: Ignoring unsupported "
+                                "protocol: %d - %s\n",
+                                data->protocol,
+                                p->p_name);
+
+                        return;
+                }
+
+                fprintf(stderr,
+                        "ERROR: socket() call failed unexpectedly.\n");
+
+                exit(EXIT_FAILURE);
+        }
+
+        bool const verified = verify_protocol(fd, data->expect_mptcp);
+
         close(fd);
 
-        assert(ret == 0
-               && protocol == expected_protocol
-               && len == sizeof(protocol));
+        assert(verified);
+}
+
+int main()
+{
+        /*
+          libmptcpwrap.so should be preloaded when running this
+          program, e.g.:
+
+          LD_PRELOAD=libmptcpwrap.so ./mptcpwrap-tester
+        */
+        char const *const LD_PRELOAD = getenv("LD_PRELOAD");
+        assert(LD_PRELOAD != NULL);
+        assert(strstr(LD_PRELOAD, "libmptcpwrap.so") != NULL);
+
+        /*
+          MPTCP is only injected when using the SOCK_STREAM socket
+          type and a protocol value of 0 or IPPROTO_TCP.
+        */
+        static struct socket_data const data[] = {
+                { AF_LOCAL, SOCK_STREAM, 0,            false },
+                { AF_INET,  SOCK_STREAM, IPPROTO_SCTP, false },
+                { AF_INET,  SOCK_DGRAM,  0,            false },
+                { AF_INET,  SOCK_STREAM, 0,            true  },
+                { AF_INET6, SOCK_STREAM, 0,            true  },
+                { AF_INET,  SOCK_STREAM, IPPROTO_TCP,  true  },
+                { AF_INET6, SOCK_STREAM, IPPROTO_TCP,  true  }
+        };
+
+        static size_t const len = sizeof(data) / sizeof(data[0]);
+
+        for (size_t i = 0; i < len; ++i) {
+                fprintf(stderr, "Test case %zu: ", i);
+                test_socket_data(&data[i]);
+                fprintf(stderr, "PASS\n");
+        }
 
         return 0;
 }
