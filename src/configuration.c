8af992f9ad6f5ae749e808677398f075806ed2ac
João Meira <dulive.misc@protonmail.com>
2021-11-14 16:00:50 +0000

    fixed set_plugins_to_load mem leak

--- src/configuration.c
+++ src/configuration.c
@@ -325,10 +325,12 @@ static void set_plugins_to_load(struct mptcpd_config *config,
         while (token) {
                 l_queue_push_tail(
                         (struct l_queue *) config->plugins_to_load,
-                        token);
+                        strdup(token));
 
                 token = strtok(NULL, ",");
         }
+
+        l_free((char *) plugins);
 }
 
 // ---------------------------------------------------------------
@@ -813,7 +815,7 @@ struct mptcpd_config *mptcpd_config_create(int argc, char *argv[])
                 && check_config(config);
 
         l_queue_destroy((struct l_queue *) sys_config.plugins_to_load,
-                        NULL);
+                        l_free);
         l_free((char *) sys_config.default_plugin);
         l_free((char *) sys_config.plugin_dir);
 
@@ -860,7 +862,7 @@ void mptcpd_config_destroy(struct mptcpd_config *config)
                 return;
 
         l_queue_destroy((struct l_queue *) config->plugins_to_load,
-                        NULL);
+                        l_free);
         l_free((char *) config->default_plugin);
         l_free((char *) config->plugin_dir);
         l_free(config);
