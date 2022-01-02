0a1adf12be891372f4ef5eae4d0defeefc686b8f
Ossama Othman <ossama.othman@intel.com>
2022-01-02 13:47:15 -0800

    tests: Setup test address before command test run.
    The mptcpd_kpm_add_addr() and mptcpd_kpm_get_addr() command tests will
    fail if the test IP address is not bound to a network interface.  Set
    up the test IP address on the loopback interface prior to the test
    run, and tear it down afterward.  This requires that the test be run
    with CAP_NET_ADMIN capability, such as running it through the root
    user.
    
    The previous behavior of ignoring and logging failures due to lack of
    the CAP_NET_ADMIN capability is still preserved.

--- tests/test-commands.c
+++ tests/test-commands.c
@@ -11,16 +11,25 @@
 #include <unistd.h>
 #include <assert.h>
 #include <errno.h>
+#include <error.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <net/if.h>
 
 #include <ell/main.h>
 #include <ell/idle.h>
 #include <ell/util.h>      // Needed by <ell/log.h>
 #include <ell/log.h>
+#include <ell/netlink.h>
+#include <ell/rtnl.h>
 #include <ell/test.h>
 
-#include <mptcpd/private/configuration.h>  // INTERNAL!
-#include "../src/path_manager.h"           // INTERNAL!
+// Internal Headers
+// -----------------
+#include <mptcpd/private/configuration.h>
+#include "../src/path_manager.h"
+#include "../src/commands.h"
+// -----------------
 
 #include "test-plugin.h"
 #include "test-util.h"
@@ -34,16 +43,33 @@
 
 struct test_info
 {
-        struct mptcpd_pm *const pm;
+        struct l_netlink *const rtnl;
+
+        // Address used for kernel add_addr and dump_addr calls.
+        struct sockaddr const *const addr;
+
+        // Network interface on which to bind the test address.
+        int const ifindex;
+
+        // CIDR prefix
+        uint8_t const prefix;
+
+        // String form of the addr.
+        char const *const ip;
+
+        // Mptcpd configuration.
+        struct mptcpd_config *config;
+
+        // Mptcpd path manager object.
+        struct mptcpd_pm *pm;
 
         // ID used for kernel add_addr and dump_addr calls.
         mptcpd_aid_t id;
 
-        // Address used for kernel add_addr and dump_addr calls.
-        struct sockaddr const *addr;
-
+        // Number of times dump_addrs call was completed.
         int dump_addrs_complete_count;
 
+        // Were the tests called?
         bool tests_called;
 };
 
@@ -93,8 +119,9 @@ static void get_addr_callback(struct mptcpd_addr_info const *info,
         assert(info != NULL);
 
         assert(mptcpd_addr_info_get_id(info) == tinfo->id);
+        assert(mptcpd_addr_info_get_index(info) == tinfo->ifindex);
         assert(sockaddr_is_equal(tinfo->addr,
-                                 (struct sockaddr *) &info->addr));
+                                 mptcpd_addr_info_get_addr(info)));
 }
 
 static void dump_addrs_callback(struct mptcpd_addr_info const *info,
@@ -114,8 +141,9 @@ static void dump_addrs_callback(struct mptcpd_addr_info const *info,
         if (mptcpd_addr_info_get_id(info) != tinfo->id)
                 return;
 
+        assert(mptcpd_addr_info_get_index(info) == tinfo->ifindex);
         assert(sockaddr_is_equal(tinfo->addr,
-                                 (struct sockaddr *) &info->addr));
+                                 mptcpd_addr_info_get_addr(info)));
 }
 
 static void dump_addrs_complete(void *user_data)
@@ -135,7 +163,7 @@ static void get_limits_callback(struct mptcpd_limit const *limits,
         if (geteuid() != 0) {
                 /*
                   If the current user is not root, the previous
-                  set_limits() call fails with ENOPERM, but libell
+                  set_limits() call fails with EPERM, but libell
                   APIs don't allow reporting such error to the caller.
                   Just assume set_limits() has no effect.
                 */
@@ -183,17 +211,15 @@ static void test_add_addr(void const *test_data)
         assert(result == 0 || result == ENOTSUP);
 
         // In-kernel (server-oriented) path manager.
-        info->addr = laddr1;
-        info->id   = mptcpd_idm_get_id(idm, info->addr);
+        info->id = mptcpd_idm_get_id(idm, info->addr);
 
         uint32_t flags = 0;
-        int index = 0;
 
         result = mptcpd_kpm_add_addr(pm,
                                      info->addr,
                                      info->id,
                                      flags,
-                                     index);
+                                     info->ifindex);
 
         assert(result == 0 || result == ENOTSUP);
 }
@@ -225,7 +251,8 @@ static void test_get_addr(void const *test_data)
                 mptcpd_kpm_get_addr(pm,
                                     info->id,
                                     get_addr_callback,
-                                    info,NULL);
+                                    info,
+                                    NULL);
 
         assert(result == 0 || result == ENOTSUP);
 }
@@ -265,6 +292,12 @@ static void test_set_limits(void const *test_data)
         struct test_info *const info = (struct test_info *) test_data;
         struct mptcpd_pm *const pm   = info->pm;
 
+        /**
+         * @todo This call potentially overrides previously set MPTCP
+         *       limits and leaves them in place after process exit.
+         *       It would probably be good to restore previous
+         *       values.
+         */
         int const result = mptcpd_kpm_set_limits(pm,
                                                  _limits,
                                                  L_ARRAY_SIZE(_limits));
@@ -291,7 +324,7 @@ static void test_set_flags(void const *test_data)
 
         static mptcpd_flags_t const flags = MPTCPD_ADDR_FLAG_BACKUP;
 
-        int const result = mptcpd_kpm_set_flags(pm, laddr1, flags);
+        int const result = mptcpd_kpm_set_flags(pm, info->addr, flags);
 
         assert(result == 0 || result == ENOTSUP);
 }
@@ -349,6 +382,64 @@ void test_get_nm(void const *test_data)
 
 // -------------------------------------------------------------------
 
+static void handle_rtm_newaddr(int errnum,
+                               uint16_t type,
+                               void const *data,
+                               uint32_t len,
+                               void *user_data)
+{
+        /*
+          No data expected in underlying RTM_NEWADDR command reply
+          message.
+        */
+        assert(type == 0);
+        assert(data == NULL);
+        assert(len == 0);
+
+        if (errnum != 0) {
+
+                static int const status = 0;  // Do not exit on error.
+
+                struct test_info *const info = user_data;
+
+                error(status,
+                      errnum,
+                      "bind of test address %s to interface %d failed",
+                      info->ip,
+                      info->ifindex);
+        }
+}
+
+static void handle_rtm_deladdr(int errnum,
+                               uint16_t type,
+                               void const *data,
+                               uint32_t len,
+                               void *user_data)
+{
+        /*
+          No data expected in underlying RTM_DELADDR command reply
+          message.
+        */
+        assert(type == 0);
+        assert(data == NULL);
+        assert(len == 0);
+
+        if (errnum != 0) {
+                static int const status = 0;  // Do not exit on error.
+
+                struct test_info *const info = user_data;
+
+                error(status,
+                      errnum,
+                      "unbind of test address %s from "
+                      "interface %d failed",
+                      info->ip,
+                      info->ifindex);
+        }
+}
+
+// -------------------------------------------------------------------
+
 static void run_tests(struct mptcpd_pm *pm, void *user_data)
 {
         (void) pm;
@@ -360,10 +451,95 @@ static void run_tests(struct mptcpd_pm *pm, void *user_data)
         t->tests_called = true;
 }
 
+static struct mptcpd_pm_ops const pm_ops = { .ready = run_tests };
+
+static void setup_tests (void *user_data)
+{
+        struct test_info *const info = user_data;
+
+        static char *argv[] = {
+                "test-commands",
+                "--plugin-dir",
+                TEST_PLUGIN_DIR
+        };
+        static char **args = argv;
+
+        static int argc = L_ARRAY_SIZE(argv);
+
+        info->config = mptcpd_config_create(argc, argv);
+        assert(info->config);
+
+        info->pm = mptcpd_pm_create(info->config);
+        assert(info->pm);
+
+        bool const registered =
+                mptcpd_pm_register_ops(info->pm, &pm_ops, info);
+
+        assert(registered);
+
+        l_test_init(&argc, &args);
+
+        l_test_add("add_addr",       test_add_addr,       info);
+        l_test_add("get_addr",       test_get_addr,       info);
+        l_test_add("dump_addrs",     test_dump_addrs,     info);
+        l_test_add("remove_addr",    test_remove_addr,    info);
+        l_test_add("set_limits",     test_set_limits,     info);
+        l_test_add("get_limits",     test_get_limits,     info);
+        l_test_add("set_flags",      test_set_flags,      info);
+        l_test_add("flush_addrs",    test_flush_addrs,    info);
+        l_test_add("add_subflow",    test_add_subflow,    info);
+        l_test_add("set_backup",     test_set_backup,     info);
+        l_test_add("remove_subflow", test_remove_subflow, info);
+        l_test_add("get_nm",         test_get_nm,         info);
+}
+
+static void complete_address_setup(void *user_data)
+{
+        // Run tests after address setup is complete.
+        bool const result = l_idle_oneshot(setup_tests, user_data, NULL);
+        assert(result);
+}
+
+static void complete_address_teardown(void *user_data)
+{
+        (void) user_data;
+
+        l_main_quit();
+}
+
+static void setup_test_addresses(struct test_info *info)
+{
+        int const id = l_rtnl_ifaddr4_add(info->rtnl,
+                                          info->ifindex,
+                                          info->prefix,
+                                          info->ip,
+                                          NULL, // broadcast
+                                          handle_rtm_newaddr,
+                                          info,
+                                          complete_address_setup);
+
+        assert(id != 0);
+}
+
+static void teardown_test_addresses(struct test_info *info)
+{
+        int const id = l_rtnl_ifaddr4_delete(info->rtnl,
+                                             info->ifindex,
+                                             info->prefix,
+                                             info->ip,
+                                             NULL, // broadcast
+                                             handle_rtm_deladdr,
+                                             info,
+                                             complete_address_teardown);
+
+        assert(id != 0);
+}
+
+// -------------------------------------------------------------------
+
 static void idle_callback(struct l_idle *idle, void *user_data)
 {
         (void) idle;
-        (void) user_data;
 
         /*
           Number of ELL event loop iterations to go through before
@@ -387,7 +563,7 @@ static void idle_callback(struct l_idle *idle, void *user_data)
         assert(max_count > trigger_count);  // Sanity check.
 
         if (++count > max_count)
-                l_main_quit();
+                teardown_test_addresses(user_data); // Done running tests.
 }
 
 // -------------------------------------------------------------------
@@ -400,50 +576,42 @@ int main(void)
         l_log_set_stderr();
         l_debug_enable("*");
 
-        static char *argv[] = {
-                "test-commands",
-                "--plugin-dir",
-                TEST_PLUGIN_DIR
-        };
-        static char **args = argv;
+        struct l_netlink *const rtnl = l_netlink_new(NETLINK_ROUTE);
+        assert(rtnl != NULL);
 
-        static int argc = L_ARRAY_SIZE(argv);
+        static struct sockaddr const *const sa = laddr1;
 
-        struct mptcpd_config *const config =
-                mptcpd_config_create(argc, argv);
-        assert(config);
+        // Bind test IP address to loopback interface.
+        static char const loopback[] = "lo";
 
-        struct mptcpd_pm *pm = mptcpd_pm_create(config);
-        assert(pm);
+        char ip[INET6_ADDRSTRLEN] = { 0 };
 
-        static struct mptcpd_pm_ops const pm_ops = { .ready = run_tests };
+        uint8_t prefix = 0;
+        void const *src = NULL;
+
+        if (sa->sa_family == AF_INET) {
+                prefix = 32;   // One IPv4 address
+                src = &((struct sockaddr_in  const *) sa)->sin_addr;
+        } else {
+                prefix = 128;  // One IPv6 address
+                src = &((struct sockaddr_in6 const *) sa)->sin6_addr;
+        }
 
         struct test_info info = {
-                .pm = pm
+                .rtnl    = rtnl,
+                .addr    = sa,
+                .ifindex = if_nametoindex(loopback),
+                .prefix  = prefix,
+                .ip      = inet_ntop(sa->sa_family,
+                                     src,
+                                     ip,
+                                     sizeof(ip))
         };
 
-        bool const registered =
-                mptcpd_pm_register_ops(pm, &pm_ops, &info);
-
-        assert(registered);
-
-        l_test_init(&argc, &args);
-
-        l_test_add("add_addr",       test_add_addr,       &info);
-        l_test_add("get_addr",       test_get_addr,       &info);
-        l_test_add("dump_addrs",     test_dump_addrs,     &info);
-        l_test_add("remove_addr",    test_remove_addr,    &info);
-        l_test_add("set_limits",     test_set_limits,     &info);
-        l_test_add("get_limits",     test_get_limits,     &info);
-        l_test_add("set_flags",      test_set_flags,      &info);
-        l_test_add("flush_addrs",    test_flush_addrs,    &info);
-        l_test_add("add_subflow",    test_add_subflow,    &info);
-        l_test_add("set_backup",     test_set_backup,     &info);
-        l_test_add("remove_subflow", test_remove_subflow, &info);
-        l_test_add("get_nm",         test_get_nm,         &info);
+        setup_test_addresses(&info);
 
         struct l_idle *const idle =
-                l_idle_create(idle_callback, NULL, NULL);
+                l_idle_create(idle_callback, &info, NULL);
 
         (void) l_main_run();
 
@@ -456,8 +624,9 @@ int main(void)
         assert(info.dump_addrs_complete_count == 1);
 
         l_idle_remove(idle);
-        mptcpd_pm_destroy(pm);
-        mptcpd_config_destroy(config);
+        l_netlink_destroy(rtnl);
+        mptcpd_pm_destroy(info.pm);
+        mptcpd_config_destroy(info.config);
 
         return l_main_exit() ? 0 : -1;
 }
