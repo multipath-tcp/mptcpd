af5eee06d9642b6e1a55ffcc1266d78be43ed070
Ossama Othman <ossama.othman@intel.com>
2022-07-14 08:26:48 -0700

    tests: Use new API in test-listener-manager.

--- tests/test-listener-manager.c
+++ tests/test-listener-manager.c
@@ -22,8 +22,7 @@
 #include <assert.h>
 
 
-static struct l_hashmap *_lm;
-static mptcpd_aid_t const _id = 245;
+static struct mptcpd_lm *_lm;
 
 static void test_create(void const *test_data)
 {
@@ -36,29 +35,16 @@ static void test_create(void const *test_data)
 
 static void test_listen(void const *test_data)
 {
-        (void) test_data;
+        struct sockaddr const *const sa = test_data;
 
-        /*
-          Listen on the loopback address since we need an address
-          backed by a network interface so that underlying bind() call
-          can succeed.
-        */
-        struct sockaddr_in addr = {
-            .sin_family = AF_INET,
-            .sin_addr   = { .s_addr = htonl(INADDR_LOOPBACK) }
-        };
-
-        struct sockaddr const *const sa =
-            (struct sockaddr const *) &addr;
-
-        assert(mptcpd_lm_listen(_lm, _id, sa));
+        assert(mptcpd_lm_listen(_lm, sa));
 }
 
 static void test_close(void const *test_data)
 {
-        (void) test_data;
+        struct sockaddr const *const sa = test_data;
 
-        assert(mptcpd_lm_close(_lm, _id));
+        assert(mptcpd_lm_close(_lm, sa));
 }
 
 
@@ -75,10 +61,40 @@ int main(int argc, char *argv[])
 
         l_test_init(&argc, &argv);
 
-        l_test_add("create listener manager",  test_create,    NULL);
-        l_test_add("listen",                   test_listen,    NULL);
-        l_test_add("close",                    test_close,     NULL);
-        l_test_add("destroy listener manager", test_destroy,   NULL);
+        /*
+          Listen on the loopback address since we need an address
+          backed by a network interface so that underlying bind() call
+          can succeed.
+        */
+        struct sockaddr_in addr4 = {
+            .sin_family = AF_INET,
+            .sin_addr   = { .s_addr = htonl(INADDR_LOOPBACK) }
+        };
+
+        struct sockaddr const *const sa4 =
+            (struct sockaddr const *) &addr4;
+
+        /*
+          Listen on the loopback address since we need an address
+          backed by a network interface so that underlying bind() call
+          can succeed.
+        */
+        struct sockaddr_in6 addr6 = {
+            .sin6_family = AF_INET6
+        };
+
+        addr6.sin6_addr = in6addr_loopback;
+
+        struct sockaddr const *const sa6 =
+            (struct sockaddr const *) &addr6;
+
+        l_test_add("create listener manager",  test_create,  NULL);
+        l_test_add("listen - IPv4 - FIRST",    test_listen,  sa4);
+        l_test_add("listen - IPv6",            test_listen,  sa6);
+        l_test_add("listen - IPv4 - SECOND",   test_listen,  sa4);
+        l_test_add("close  - IPv4",            test_close,   sa4);
+        l_test_add("close  - IPv6",            test_close,   sa6);
+        l_test_add("destroy listener manager", test_destroy, NULL);
 
         return l_test_run();
 }
